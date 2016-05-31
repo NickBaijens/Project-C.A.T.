6a73e44e3656e2e23b9a622e7491ac6e27d0bc9e
NickBaijens <nbnickbaijens@gmail.com>
2016-05-31 10:29:19 +0200

    Worldmap + 2 tiles
    Worldmap now working, map can be changed by editing the map array. added
    2 tiles for testing

--- Last_Quest_HaxeFlixel/source/Tile.hx
+++ Last_Quest_HaxeFlixel/source/Tile.hx
@@ -10,13 +10,13 @@ import flixel.system.FlxAssets.FlxGraphicAsset;
  */
 class Tile extends FlxSprite
 {
-	static var types = ["Grassland tile 64x64.png", "tile calm water 64x64.png"];
+	static var types = ["Grassland tile 64x64.png", "tile sea water2.png"];
 	var type: String = types[1];
 	public function new(i:Int) 
 	{
 		super();
 		type = types[i];
-		loadGraphic("assets/images/"+type);
+		loadGraphic("assets/images/tiles/"+type);
 		PlayState.instance.add(this);		
 	}
 }
