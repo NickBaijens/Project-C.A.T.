a3fa8d1182ed7c104a0a9f0d90b53ad5672a9c7c
Dave Bechtum <dbechtum@gmail.com>
2016-05-31 10:42:01 +0200

    D_53116
    Initial work on bank

--- Last_Quest_HaxeFlixel/source/Bank.hx
+++ Last_Quest_HaxeFlixel/source/Bank.hx
@@ -7,10 +7,38 @@ import flash.events.Event;
  */
 class Bank
 {
-	public var bank : Array <Resource> = new Array();
+	public var bankInv : Array <Int> = new Array();
+	public var water : Int = 0;//0
+	public var food : Int = 0;//1
+	public var stone : Int = 0;//2
+	public var wood : Int = 0;//3
+	public var iron : Int = 0;//4
+	public var clay : Int = 0;//5
 	public function new() 
 	{
-		
+		bankInv = [0, 0, 5, 5, 4, 3, 2, 1, 0, 0];
+		trace(bankInv);
+		bankInv.sort(function(a,b) return a-b);
+		trace(bankInv);
+		for (i in bankInv)
+		{
+			trace(bankInv[i]);
+			switch bankInv[i]{	
+				
+				case 0:water += 1;
+				case 1:food += 1;
+				case 2:stone += 1;
+				case 3:wood += 1;
+				case 4:iron += 1;
+				case 5:clay += 1;
+			}
+		}
+		trace(water);
+		trace(food);
+		trace(stone);
+		trace(wood);
+		trace(iron);
+		trace(clay);
 	}
 	public function updateBank(e:Event):Void
 	{
--- Last_Quest_HaxeFlixel/source/Game.hx
+++ Last_Quest_HaxeFlixel/source/Game.hx
@@ -1,5 +1,7 @@
 package;
 import Resource;
+import WorldMap;
+import Bank;
 
 /**
  * ...
@@ -10,6 +12,8 @@ class Game
 	
 	public function new() 
 	{
+		var map : WorldMap = new WorldMap();
+		var bank : Bank = new Bank();
 		var resource : Resource = new Resource(); // creates initial instance of Resource Class
 		resource.generateRandomResource(200, 200); // testing creation of random resource
 		resource.generateResource(1, 300, 300); // testing creation of selected resource
