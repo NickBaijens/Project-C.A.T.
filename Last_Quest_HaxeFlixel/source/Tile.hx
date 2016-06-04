5e8bb9842083171ad6acbdb477dde85f8de717a0
NickBaijens <nbnickbaijens@gmail.com>
2016-06-04 21:39:40 +0200

    Added back code I didn't notice in merge conflict
    GG Github...=.='

--- Last_Quest_HaxeFlixel/source/Tile.hx
+++ Last_Quest_HaxeFlixel/source/Tile.hx
@@ -9,7 +9,9 @@ import flixel.input.mouse.FlxMouseEventManager;
 
 class Tile extends FlxSprite
 {
-	static var types = ["Grassland tile 64x64.png", "tile sea water2.png"];
+	static var types = ["Grassland tile 64x64.png", "Sea tile 64x64.png", "Dessertland tile 64x64.png", "Rockland tile 64x64.png", "Town NW tile 64x64.png", "Town NE tile 64x64.png", "Town SE tile 64x64.png", "Town SW tile 64x64.png","WaterCalm tile 64x64.png"];
+	public var tileInv : Array <Int> = new Array();
+	var discovered : Bool = false;
 	var type: String = types[1];
 	public var instanceID: Int = 0;
 	
@@ -18,8 +20,19 @@ class Tile extends FlxSprite
 		super();
 		
 		type = types[tiletype];
-		loadGraphic("assets/images/tiles/"+type);
+		if (type == "Town NW tile 64x64.png" || type == "Town NE tile 64x64.png" || type == "Town SE tile 64x64.png" || type == "Town SW tile 64x64.png")
+		{
+			loadGraphic("assets/images/tiles/" + type);
+		} else if (discovered == true)
+		{
+			loadGraphic("assets/images/tiles/" + type);
+		} else if (discovered == false)
+		{
+			loadGraphic("assets/images/tiles/Dessertland tile 64x64.png");
+		}
 		PlayState.instance.add(this);
+		
+		tileInv.push(0); // test adding water to inventory
 		FlxMouseEventManager.add(this, onDown, null, null, null);
 		
 	}
