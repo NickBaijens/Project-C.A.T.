555f25d2c0a3efd875a121234f52f3a95c702bbf
Dave Bechtum <dbechtum@gmail.com>
2016-06-06 12:20:46 +0200

    D_060616
    Refactored Resource

--- Last_Quest_HaxeFlixel/source/Resource.hx
+++ Last_Quest_HaxeFlixel/source/Resource.hx
@@ -20,7 +20,7 @@ class Resource extends FlxSprite
 	{
 		instance = this;
 		super();
-		resourceGenID = [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 5, 5]; //array of resources, those that are in here more often have a higher chance of being selected
+		resourceGenID = [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 5, 5, 6, 6, 6 ,6 ,6 ,6 ,6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6]; //array of resources, those that are in here more often have a higher chance of being selected
 		//ID 0 = water, 1 = food, 2 = stone, 3 = wood, 4 = iron, 5 = clay, 6 = nothing
 	}
 	public function generateRandomResource() //picks ("generates") a 'random' resource using an array and number generator
@@ -49,31 +49,6 @@ class Resource extends FlxSprite
 		{
 			trace("You didn't get anything");
 			generatedResource = 6;
-		}
-	}
-	public function generateResource(ID:Int, x:Int, y:Int)//ID 0 = water, 1 = food, 2 = stone, 3 = wood, 4 = iron, 5 = clay
-	{
-		var genResource = ID;
-		var posX = x;
-		var posY = y;
-		if (genResource == 0) //creates instance of resource with the fetched ID
-		{
-			var water : Water = new Water(posX, posY);
-		} else if (genResource == 1)
-		{
-			var food : Food = new Food(posX, posY);
-		} else if (genResource == 2)
-		{
-			var stone : Stone = new Stone(posX, posY);
-		} else if (genResource == 3)
-		{
-			var wood : Wood = new Wood(posX, posY);
-		} else if (genResource == 4)
-		{
-			var iron : Iron = new Iron(posX, posY);
-		} else if (genResource == 5)
-		{
-			var clay : Clay = new Clay(posX, posY);
 		}
 	}
 }
