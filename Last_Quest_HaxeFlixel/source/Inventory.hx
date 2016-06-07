759922c279c3fb7d1fb0280b21c06d411125ca15
Dave Bechtum <dbechtum@gmail.com>
2016-06-07 14:10:24 +0200

    D_060716
    Inventory Update

--- Last_Quest_HaxeFlixel/source/Inventory.hx
+++ Last_Quest_HaxeFlixel/source/Inventory.hx
@@ -1,18 +1,33 @@
 package;
-
+import flixel.FlxSprite;
+import flixel.ui.FlxButton;
 /**
  * ...
  * @author Dave
  */
-class Inventory extends Button
+class Inventory extends FlxSprite
 {
-
+	
+	var inventoryBttns: Array<FlxButton> = new Array<FlxButton>();
+	var slots: Array<Int> = new Array<Int>(9);
 	public function new() 
 	{
 		super();
+		
+		for (y in 0...3)
+		{
+			for (x in 0...3)
+			{
+				var invbttn = new FlxButton(x * 80, y * 20, "", buttonPressed);
+				invbttn.x += 50;
+				invbttn.y += 50;
+				PlayState.instance.add(invbttn);
+				inventoryBttns.push(invbttn);
+			}
+		}
 	}
 	
-	override public function buttonPressed()
+	function buttonPressed():Void
 	{
 		
 	}
